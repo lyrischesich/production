d4f9f6e7020ed9033cf436a1e3cd99a1eec974a1
root <root@Hanniserver.(none)>
2014-01-27 11:20:56 +0100

    Database Experiments

--- app/Config/database.php
+++ app/Config/database.php
@@ -64,7 +64,8 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
+		//'host' => 'localhost',
+		'host' => 'nations.no-ip.org',
 		'login' => 'cafeteria',
 		'password' => 'apfelkuchen1',
 		'database' => 'cafeteria_dev',
