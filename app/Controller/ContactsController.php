1d625423b9703293727a488112929b21194659ed
Johannes <jograeger@gmail.com>
2014-02-13 10:43:42 +0100

    Arbeiten an der E-Mail Funktion in der Kontaktliste wurden fortgesetzt.

--- app/Controller/ContactsController.php
+++ app/Controller/ContactsController.php
@@ -1,53 +1,63 @@
 <?php
-	class ContactsController extends AppController {
-		public $uses = array('User');
-		public $helpers = array('Js');
-		public $components = array('Paginator','Session');
-		
-		public $paginate = array(
-				'fields' => array('User.lname','User.fname','User.tel1','User.tel2','User.mail','User.mo','User.di','User.mi','User.do','User.fr','User.leave_date'),
-				'limit' => 25,
-				'order' => array(
-					'User.lname' => 'asc'	
-					),
-				);
-		
-		public function index() {
-			$this->Paginator->settings = $this->paginate;
+class ContactsController extends AppController {
+	public $uses = array('User');
+	public $helpers = array('Js');
+	public $components = array('Paginator','Session');
+
+	public $paginate = array(
+			'fields' => array('User.lname','User.fname','User.tel1','User.tel2','User.mail','User.mo','User.di','User.mi','User.do','User.fr','User.leave_date'),
+			'limit' => 25,
+			'order' => array(
+					'User.lname' => 'asc'
+			),
+	);
+
+	public function index() {
+		$this->Paginator->settings = $this->paginate;
 			
-			$results = $this->Paginator->paginate();
-			foreach ($results as &$result) {
-				//Hier wird die CSS-Klasse für die jeweiligen Tage in das Array eingebunden
-				$result['User']['mo'] = $this->addStyle($result['User']['mo']);
-				$result['User']['di'] = $this->addStyle($result['User']['di']);
-				$result['User']['mi'] = $this->addStyle($result['User']['mi']);
-				$result['User']['do'] = $this->addStyle($result['User']['do']);
-				$result['User']['fr'] = $this->addStyle($result['User']['fr']);	
-			}
-			$this->set('users',$results);
+		$results = $this->Paginator->paginate();
+		foreach ($results as &$result) {
+			//Hier wird die CSS-Klasse fï¿½r die jeweiligen Tage in das Array eingebunden
+			$result['User']['mo'] = $this->addStyle($result['User']['mo']);
+			$result['User']['di'] = $this->addStyle($result['User']['di']);
+			$result['User']['mi'] = $this->addStyle($result['User']['mi']);
+			$result['User']['do'] = $this->addStyle($result['User']['do']);
+			$result['User']['fr'] = $this->addStyle($result['User']['fr']);
 		}
-		
-		/**
-		 * Die Zellen der Tabelle in den Spalte [mo,di,mi,do,fr] sollen verschiedenfarbig unterlegt sein,
-		 * je nachdem wie die betreffende Person am entsprechenden Wochentag Zeit hat. Diese Methode erwartet
-		 * ein Ergebnis aus der Datenbank und gibt ein array zurück, welches neben dem Ergebnis auch eine CSS-Klasse enthält,
-		 * die in die View eingebettet werden kann.
-		 * @param Ergebnis aus der Datebank, welches einem Wert des enums entspricht $day
-		 */
-		private function addStyle($dayValue = null) {
-			if (strcmp($dayValue,"G") == 0) {
-				return array('value' => $dayValue,'class' => 'success');
-			} elseif (strcmp($dayValue,"H") == 0 || strcmp($dayValue,"1") == 0 || strcmp($dayValue,"2") == 0) {
-				return array('value' => $dayValue,'class' => 'warning');
-			} elseif (strcmp($dayValue,"N") ==0) {
-				return array('value' => $dayValue,'class' => 'error');
-			}
-		}
-		
-		public function mail($to = null) {
-			if ($this->request->is('get')) {
-				
-			}
+		$this->set('users',$results);
+	}
+
+	/**
+	 * Die Zellen der Tabelle in den Spalte [mo,di,mi,do,fr] sollen verschiedenfarbig unterlegt sein,
+	 * je nachdem wie die betreffende Person am entsprechenden Wochentag Zeit hat. Diese Methode erwartet
+	 * ein Ergebnis aus der Datenbank und gibt ein array zurï¿½ck, welches neben dem Ergebnis auch eine CSS-Klasse enthï¿½lt,
+	 * die in die View eingebettet werden kann.
+	 * @param Ergebnis aus der Datebank, welches einem Wert des enums entspricht $day
+	 */
+	private function addStyle($dayValue = null) {
+		if (strcmp($dayValue,"G") == 0) {
+			return array('value' => $dayValue,'class' => 'success');
+		} elseif (strcmp($dayValue,"H") == 0 || strcmp($dayValue,"1") == 0 || strcmp($dayValue,"2") == 0) {
+			return array('value' => $dayValue,'class' => 'warning');
+		} elseif (strcmp($dayValue,"N") ==0) {
+			return array('value' => $dayValue,'class' => 'error');
 		}
 	}
+	
+	/**
+	 * Erwartet ein GET-Parameter und setzt den Wert des Adressfeldes entsprechend, fÃ¼r den eigentlichen
+	 * E-Mail Versand ist eine andere Methode zustÃ¤ndig.
+	 * @param String $to = Der EmpfÃ¤nger, der im Adressfeld angezeigt werden soll
+	 */
+	public function mail($data = null) {
+		if ($this->request->is('get')) {
+			$this->set('receiver',$data);
+		}
+		if ($this->request->is('post')) {
+			$receivers = $this->User->validateReceivers($this->request->data);
+		}
+
+	}
+
+}
 ?>
--- app/View/Contacts/mail.ctp
+++ app/View/Contacts/mail.ctp
@@ -1,3 +1,19 @@
+<script type="text/javascript">
+	$(document).ready(function() {
+		//Gleiche die Breite der Textarea mit der des Empfängerfeldes an
+		var width = $("#MailMailTo").width();
+		$("#MailContent").width(width);
+		$("#MailContent").height(120);
+
+		//Versehe den Button "Felder leeren" mit einer Funktion
+		$("#clear-fields").click(function() {
+			$("#MailMailTo").val("");
+			$("#MailContent").val("");
+		});	
+		
+	});
+</script>
+
 <div class="span2">
 	<div class="well sidebar-nav">
 		<ul class="nav nav-list">
@@ -19,6 +35,42 @@
 	können, werden nicht akzeptiert. Sie können als Empfänder sowohl den <b>Vor- und Nachnamen</b>, als auch eine gültige <b>E-Mail-Adresse</b> angeben.
 	Um eine E-Mail an alle Mitarbeiter, oder bestimmte Mitarbeitergruppen zu versenden, wählen sie bitte die Option "Rundmail verschicken" in der Seitenleiste.
 	</p>
+	<p>
+	Wenn sie E-Mails an mehrere Mitarbeiter versenden wollen, so trennen sie bitte deren Namen bzw. Mail-Adressen mit einem <b>Semikolon(;)</b>
+	</p>
 	<br>
-	
+	<?php echo "<legend>E-Mail versenden</legend>"; ?>
+	<?php echo $this->Form->create('Mail',array(
+			'type' => 'post',
+			'url' => array('controller' => 'contacts', 'action' => 'mail'),
+			'inputDefaults' => array(
+					'div' => 'control-group',
+					'label' => array(
+							'class' => 'control-label'
+					 ),
+			'class' => 'well form-horizontal')
+			)); ?>
+		<?php echo $this->Form->input('mailTo',array(
+				'placeholder' => 'Empfänger',
+				'div' => 'control-group',
+				'class' => 'input-xxlarge',
+				'label' => array(
+						'text' => 'An:',
+					),
+				'value' => $receiver,
+				'beforeInput' => '<div class="input-prepend"><span class="add-on"><i class="icon-envelope"></i></span>',
+				'afterInput' => '</div>'
+				));?>
+		<?php echo $this->Form->textarea('content',array(
+				'placeholder' => 'Bitte geben sie den zu versendenden Text ein',
+				'label' => array ( 'text' => 'Inhalt:')				
+				));?>
+		<div class="form-actions">
+			<?php echo $this->Form->submit('E-Mail versenden',array(
+						'div' => false,
+						'class' => 'btn btn-primary'
+					));?>
+			<button type="button" class="btn" id="clear-fields">Felder leeren</button>
+		</div>
+	<?php echo $this->Form->end();?>
 </div>
