d9c9e5cb992307901d45b0440bb91ed781f10a00
milchbarplan1337 <alex.loeser@freenet.de>
2014-03-05 23:47:51 +0100

    Ermittlung von fehlenden Belegungen im Plan. Korrekte Zuordnung von fehlenden Belegungen und Arbeitszeiten der Mitarbeiter. Dies beinhaltet sowohl Text- als auch Benutzerspalten und eine Prüfung auf entsprechende Rechte

--- app/Controller/PlanController.php
+++ app/Controller/PlanController.php
@@ -72,9 +72,100 @@ class PlanController extends AppController {
 		
 	}
 	
+	public function sendMissingShiftMails(){
+		$firstDate = new DateTime('now');
+		$firstDate = $firstDate->modify("+".(8-date('N', time()))." days");
+		$tmpColumns = $this->Column->find('all', array('recursive' => -1));
+		$columns = array();
+		foreach ($tmpColumns as $tmpColumn) {
+			$columns[$tmpColumn['Column']['id']] = $tmpColumn['Column']['req_admin'];
+		}
+		
+		$data = array();
+		
+		//auf Deutsch umstellen
+		setlocale (LC_TIME, 'de_DE@euro', 'de_DE', 'de', 'ge', 'de_DE.utf8');
+		for ($i = 0;$i < 7;$i++) {
+			$currentDate = $firstDate->format("Y-m-d");
+			$missing = $this->Plan->getMissingShifts($currentDate);
+	
+			$data[$currentDate]["missing"] = ($missing === array()) ? null : $missing;
+			$data[$currentDate]["dow"] = strftime('%A', strtotime($currentDate));
+			$data[$currentDate]["weekend"] = date('N', strtotime($currentDate)) >= 6;
+			$data[$currentDate]["specialdate"] = $this->Specialdate->find("count", array("conditions" => array("Specialdate.date" => $currentDate))) === 1;
+			$firstDate->modify("+1 day");
+		}
+		
+		$users = $this->User->find("all", array("recursive" => -1, "conditions" => array("User.mail != " => "", "User.leave_date" => null)));
+		$workableUsers = array();
 
+		foreach ($data as $date => $info) {
+			//Tage, an denen nicht gearbeitet wird, werden ignoriert
+			if (($info['weekend'] && !$info['specialdate']) || (!$info['weekend'] && $info['specialdate'])) {
+				continue;
+			}
+			
+			//Es handelt sich um ein Datum, an dem gearbeitet wird
+			if (!isset($info['missing'])) {
+				//Alle Dienste sind bereits belegt -> ignorieren
+				continue;
+			}
+			
+			//Es handelt sich um ein Datum, an dem noch Schichten fehlen
+			foreach ($info['missing'] as $columnId => $shifts) {
+				//Handelt es sich um eine Benutzerspalte/Textspalte?
+				if ($shifts != null) {
+					$missingShiftNumeric = 0;
+					if (array_key_exists(1, $shifts))
+						$missingShiftNumeric += 1;
+					
+					if (array_key_exists(2, $shifts))
+						$missingShiftNumeric += 2;
+				} else {
+					//Textspalte
+					$missingShiftNumeric = -1;
+				}
+							
+				foreach ($users as $user) {
+					//Falls Adminrechte benötigt werden, muss der Nutzer diese auch besitzen, sonst weiter
+					if ($columns[$columnId] == 1 && !$user['User']['admin']) {
+						continue;
+					}
+					
+					if ($missingShiftNumeric == -1) {
+						$workableUsers[$user['User']['id']][$date][$columnId] = -1;
+					} else if ($info['weekend']) {
+						//Zu Wochenenden wurden keine Arbeitszeiten gespeichert
+						//-> Alle Mitarbeiter werden gewählt
+						$workableUsers[$user['User']['id']][$date][$columnId] = $missingShiftNumeric;
+					} else if ($missingShiftNumeric == 3) {
+						if ($user['User'][substr(strtolower($info['dow']), 0, 2)] != "N") {
+							$workableUsers[$user['User']['id']][$date][$columnId] = 3;							
+						}
+					} else if ($missingShiftNumeric == 2) {
+						if (in_array($user['User'][substr(strtolower($info['dow']), 0, 2)], array("2", "H", "G"))) {
+							$workableUsers[$user['User']['id']][$date][$columnId] = 2;
+						}
+					} else if ($missingShiftNumeric == 1) {
+						if (in_array($user['User'][substr(strtolower($info['dow']), 0, 2)], array("1", "H", "G"))) {
+							$workableUsers[$user['User']['id']][$date][$columnId] = 1;
+						}
+					}
+				}
+			}
+		}
+		
+		debug($workableUsers);
+	}
 
 	public function isAuthorized($user) {
+		if ($this->action == "sendMissingShiftMails") {
+			//Diese Methode ist zuständig für das Generieren und Versenden der Mails
+			//bezüglich fehlender Dienste im Plan und wird zu gegebener Zeit automatisch aufgerufen
+			//->Niemand darf diese Funkion über die URL aufrufen
+			return true;
+		}
+		
 		//Alle angemeldeten Benutzer dürfen den Plan einsehen
 		return true;
 	}
